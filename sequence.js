7f68853c7a9ae717a6e47693d2d079dbef8b0fc3
Eric Gorski <er.h.gorski@gmail.com>
2020-07-21 18:08:17 -0400

    if pose is two sided, not states that duration is for each side

--- sequence.js
+++ sequence.js
@@ -2,54 +2,80 @@
 
 const poses = [
     {
-        name: "Classical Sun Salutations (Surya Namaskar)",
+        name: "Surya Namaskar<br>Classical Sun salutation",
         otherSide: false,
-        description: "",
+        description:
+            "For warmup, repeat the sun salutation sequence a few times. " +
+            "Alternate the side, starting with the right foot: " +
+            "First the right food steps back after a forward fold, and the same right foot " +
+            "steps forward after downward-facing dog. Then on the left side. " +
+            "Forgetting the details? Check this <a href= https://www.youtube.com/watch?v=AbPufvvYiSw>video</a>.",
         imageRef: "./assets/aseq_SM1.png",
     },
     {
-        name: "Standing With Inhale Knee up to Hip-Height",
+        name: "Inhale: Knee Up",
         otherSide: true,
-        description: null,
+        description:
+            "Transfering weight to the standing leg, lift one knee to " +
+            "hip-height. Check that the hips remain squared: facing forward, and parallel to the ground.",
         imageRef: "./assets/aseq_kneeupwarmup1_1.png",
     },
     {
-        name: "Palms Together (Anjali Mudra), Exhale Twist",
+        name: "Exhale: Twist",
         otherSide: true,
-        description: null,
+        description:
+            "Keeping the palms together, twist over the knee that is lifted: " +
+            "when the right knee is up, twist to the right. Keep the hips suqared, and " +
+            "notice any changes in the quality of the balance on the standing leg.",
         imageRef: "./assets/aseq_kneeupwarmup1_2.png",
     },
     {
-        name: "Inhale Open Chest, Hand to the Knee, and Look Over Shoulder",
+        name: "Drop the Hands & Deepen Twist",
         otherSide: true,
         description:
-            "Step back with the (L/R) foot perpendicular to the front of the mat. The R/L foot points to the front of the mat. The hips face the long edge of the mat. With hands on the shoulders, or out to sides, hinge to the (R/L) and then, drop the R/L hand down. The L/R arm can reach for the ceiling, or be tucked behind your back, to help keep the chest open",
+            "Inhaling, drop the shoulders away from the ears, opening the chest. " +
+            "Let the hands drop diagonally, or straighted to the sides. Optionally, " +
+            "look over your shoulder: if you are twisted to the right, over the right " +
+            "lifted knee, look over the right shoudler.",
         imageRef: "./assets/aseq_kneeupwarmup1_3.png",
     },
     {
-        name: "Triangle (Trikonasana)",
+        name: "Trikonasana<br>Triangle",
         otherSide: true,
-        description: null,
+        description:
+            "Make a step back, with the back foot and the hips facing the long edge of the mat. " +
+            "Keep the front foot facing the short edge of the mat. With hands on the shoulders, or out " +
+            "to sides, hinge over the front foot, keeping the hips facing the long edge of the mat. " +
+            "Drop the from arm down toward your shin or foot. The other arm can reach for the ceiling, " +
+            "or be tucked behind your back, to help keep the chest open.",
         imageRef: "./assets/aseq-triangle.png",
     },
     {
-        name: "Revolved Triangle (Parivrtta Trikonasana)",
+        name: "Revolved Triangle",
         otherSide: true,
-        description:
-            "Tadāsana -> Inhale lift R/L foot -> Exhale step to Warrior 1 -> Inhale palms together and up -> Exhale hands down and interlaced behind the back for Humble Warrior -> Bring the R/L heel up and transfer weight to the L/R foot, optionally lifting the R/L foot and straightening the L/R leg for Warrior 3.",
+        description: null,
         imageRef: "./assets/aseq-revolved-triangle.png",
     },
     {
-        name: "Tadasana -> W1 -> humble -> heel-up -> prep for W3",
+        name: "Warrior I to Warrior III",
         otherSide: true,
-        description: null,
+        description:
+            "From standing, inhale lift the knee, exhale step back to Warrior I: the back foot " +
+            "at a 45-degree angle. Inhale palms together and up, dropping shoulders away form the ears " +
+            "and creating space around the neck. Bring the back heel up for high lunge, then start to " +
+            "transfer weight to the front foot. Optionally, practice lifting the back foot and straightening " +
+            "back leg for Warrior III.",
         imageRef: "./assets/aseq-w1-w3.png",
     },
     {
-        name: "Tadasana -> W1 -> heel-up -> W3",
+        name: "Warrior III to Half-Moon",
         otherSide: true,
         description:
-            "Same as previous, but after W3, look down, drop the R/L hand to the floor or a block, and try to use the exhale to revolve to the left. The hips stay facing the mat, and the L/R hand can optionally come behind you or reach up, after you have already found the twist.",
+            "Transition through Warrior I to Warrior III, then look down. If you are standing on " +
+            "the right foot, ground the left palm on the mat or block; bring the other hand to the shoulder. " +
+            "Use the exhale to revolve to the right (if you are standign on the right foot). The hips stay " +
+            "Pointed the back toes toward the mat, which helps keep the hips squared. Take your time to find " +
+            " the twist. You can optionally reach up with the top hand, or tuck the arm behind your back.",
         imageRef: "./assets/aseq_revolved_half_moon.png",
     },
 ];
@@ -58,45 +84,45 @@ const sequence = [
     [
         {
             pose: poses[0],
-            duration: 2,
+            duration: 6,
         },
     ],
     [
         {
             pose: poses[1],
-            duration: 3,
+            duration: 0.5,
         },
         {
             pose: poses[2],
-            duration: 1,
+            duration: 0.5,
         },
         {
             pose: poses[3],
-            duration: 4,
+            duration: 1,
         },
     ],
     [
         {
             pose: poses[4],
-            duration: 2,
+            duration: 1,
         },
     ],
     [
         {
             pose: poses[5],
-            duration: 4,
+            duration: 1,
         },
     ],
     [
         {
             pose: poses[6],
-            duration: 2,
+            duration: 1.5,
         },
     ],
     [
         {
             pose: poses[7],
-            duration: 1,
+            duration: 1.5,
         },
     ],
 ];
